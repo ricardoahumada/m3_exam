96f0832b1e8d1576e509d5142f55efb993583a07
ricardoahumada <ricardo@enmotionvalue.com>
2017-08-08 11:07:05 +0200

    Proyecto exam m3

---
+++ ncm/src/test/java/com/nmc/db/TestClienteDAO.java
@@ -0,0 +1,16 @@
+package com.nmc.db;
+
+import java.util.logging.Logger;
+
+import org.junit.BeforeClass;
+
+public class TestClienteDAO extends TestNCM {
+	private static Logger logger = Logger.getLogger("TestClienteDAO");
+
+	@BeforeClass
+	public static void setUpClass() {
+		setUpContext();
+	}
+	
+	
+}
